970a2be4da5c27a46f07f5f43a0b004fc94df1d7
Franklin Jeng <franklinjjeng@gmail.com>
2017-03-28 13:48:27 -0700

    adding powerString

---
+++ powerString/powerString.js
@@ -0,0 +1,14 @@
+/* Write a function that accepts two strings (x and y) as input
+ * and returns the computation of x ^ y
+ *
+ * Basically, Math.pow but accepts strings as arguments
+ * 
+ * Assume all input x and y are larger than 32 bits
+ *
+ * Assume all inputs are integers in string format ('' + number)
+ * 
+ */
+
+export default function(x, y) {
+
+};
