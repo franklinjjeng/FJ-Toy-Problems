cb2a26b49e3c929315a698da6a0f2d372ad62aa0
Franklin Jeng <franklinjjeng@gmail.com>
2017-05-16 11:04:15 -0700

    adding appearsTwice problem

---
+++ appearsTwice/appearsTwice.js
@@ -0,0 +1,8 @@
+/*
+I have an array where every number in the range 1...n1...n appears once except for one number which appears twice.
+Write a function for finding the number that appears twice.
+*/
+
+var apeparsTwice = function() {
+  //TODO
+}
