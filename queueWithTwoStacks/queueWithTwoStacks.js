ea5d8c79756c2b9a17bf9ba57b0880a504883706
Franklin Jeng <franklinjjeng@gmail.com>
2017-06-22 16:42:57 -0700

    adding problem queueWithTwoStacks

---
+++ queueWithTwoStacks/queueWithTwoStacks.js
@@ -0,0 +1,16 @@
+/*
+Create a queue using two stacks
+*/
+
+var Queue = function() {
+  this.inbox = [];
+  this.outbox = [];
+};
+
+Queue.prototype.enqueue = function(val) {
+  // TODO
+};
+
+Queue.prototype.dequeue = function() {
+  // TODO
+};
