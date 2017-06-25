8000756805012d6801a6ff469f07a2172d8dd192
Franklin Jeng <franklinjjeng@gmail.com>
2017-06-24 22:04:25 -0700

    adding stockPrices problem

---
+++ stockPrices/stockPrices.js
@@ -0,0 +1,14 @@
+/*
+Given an array of stock prices, write a function that
+finds and returns the best profit I could have made from
+1 purchase and 1 sale of 1 Apple stock yesterday.
+
+example:
+input: [5, 6, 4, 8, 12, 1, 7]
+output: 8 // best time to buy at 4 and sell at 12
+
+*/
+
+var stockPrices = function() {
+  // TODO
+}
