53b99805c82871730a3cc1e2bd4863551b3ae450
Franklin Jeng <franklinjjeng@gmail.com>
2017-06-27 14:53:48 -0700

    adding problem deleteNode

---
+++ deleteNode/deleteNode.js
@@ -0,0 +1,23 @@
+/*
+Given a single linked list, delete a node in the middle given only one node.
+The linked list should remain continous.
+*/
+
+function LinkedListNode(value) {
+  this.value = value;
+  this.next = null;
+}
+
+var deleteNode = function(node) {
+  // TODO
+}
+
+
+var a = new LinkedListNode('A');
+var b = new LinkedListNode('B');
+var c = new LinkedListNode('C');
+
+a.next = b;
+b.next = c;
+
+deleteNode(b);
