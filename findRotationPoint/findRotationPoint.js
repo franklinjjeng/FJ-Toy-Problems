e7ce79f0c5567e27525bb34f8d63260a433436fd
Franklin Jeng <franklinjjeng@gmail.com>
2017-06-27 13:51:05 -0700

    adding problem findRotationPoint

---
+++ findRotationPoint/findRotationPoint.js
@@ -0,0 +1,25 @@
+/*
+Write a function for finding the index of the "rotation point,"
+which is where I started working from the beginning of the dictionary.
+This array is huge (there are lots of words I don't know) so we want to be efficient here.
+
+example: 
+  var words = [
+    'ptolemaic',
+    'retrograde',
+    'supplant',
+    'undulate',
+    'xenoepist',
+    'asymptote', // <-- rotates here!
+    'babka',
+    'banoffee',
+    'engender',
+    'karpatka',
+    'othellolagkage',
+];
+
+*/
+
+var findRotationPoint = function(words) {
+  //TODO
+}
