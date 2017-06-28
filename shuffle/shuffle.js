36f3ea24d64c92e62aaccd80164d8abd3a7bd559
Franklin Jeng <franklinjjeng@gmail.com>
2017-06-27 21:56:12 -0700

    adding problem shuffle

---
+++ shuffle/shuffle.js
@@ -0,0 +1,16 @@
+/*
+Build a function to create a deck and a function that shuffles it
+*/
+
+var deck = [];
+
+var buildDeck = function(deck) {
+  // TODO
+}
+
+var shuffle = function(deck) {
+  // TODO
+}
+
+buildDeck(deck);
+shuffle(deck);
