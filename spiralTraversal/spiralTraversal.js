a231894a42c10bb596f00a0469a4009936620106
Franklin Jeng <franklinjjeng@gmail.com>
2017-06-29 09:17:11 -0700

    adding problem spiralTraversl

---
+++ spiralTraversal/spiralTraversal.js
@@ -0,0 +1,18 @@
+/*
+Traverse through a matrix (2D Array) in a spiral and output the values in that order.
+
+Example:
+input: [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+];
+
+output: [1, 2, 3, 6, 9, 8, 7, 4, 5];
+
+*/
+
+
+var spiralTraversal = function() {
+  // TODO
+}
