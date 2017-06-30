300b90fef7ccc8006edb02a3cb84bbed92fe0abf
Franklin Jeng <franklinjjeng@gmail.com>
2017-06-30 11:28:40 -0700

    adding problem addTwoHugeNumbers

---
+++ addTwoHugeNumbers/addTwoHugeNumbers.js
@@ -0,0 +1,21 @@
+/*
+Given two linked lists that represent a large number,
+add them together and represent it as a linked list
+
+example:
+// it should be noted taht the arrays are representations of a linked list with structure node.value and node.next
+For a = [9876, 5432, 1999] and b = [1, 8001], the output should be
+addTwoHugeNumbers(a, b) = [9876, 5434, 0].
+
+Explanation: 987654321999 + 18001 = 987654340000.
+
+For a = [123, 4, 5] and b = [100, 100, 100], the output should be
+addTwoHugeNumbers(a, b) = [223, 104, 105].
+
+Explanation: 12300040005 + 10001000100 = 22301040105.
+*/
+
+
+function addTwoHugeNumbers(num1, num2) {
+  // TODO
+}
