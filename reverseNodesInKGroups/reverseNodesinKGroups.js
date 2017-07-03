ab59754af18fddbfb4d3eb741285b658efd1dda8
Franklin Jeng <franklinjjeng@gmail.com>
2017-07-03 10:51:03 -0700

    adding problem reverseNodesInKGroups

---
+++ reverseNodesInKGroups/reverseNodesinKGroups.js
@@ -0,0 +1,16 @@
+/*
+Given a linked list, reverse the nodes in k groups.
+Example
+
+For l = [1, 2, 3, 4, 5] and k = 2, the output should be
+reverseNodesInKGroups(l, k) = [2, 1, 4, 3, 5];
+For l = [1, 2, 3, 4, 5] and k = 1, the output should be
+reverseNodesInKGroups(l, k) = [1, 2, 3, 4, 5];
+For l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11] and k = 3, the output should be
+reverseNodesInKGroups(l, k) = [3, 2, 1, 6, 5, 4, 9, 8, 7, 10, 11].
+
+*/
+
+var reverseNodesInKGroups = function(list, k) {
+  // TODO
+}
