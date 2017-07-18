369c2b444e20bfce43456c673a07d2d6d7564aa0
Franklin Jeng <franklinjjeng@gmail.com>
2017-07-18 10:59:46 -0700

    adding problem findInOrderedSet

---
+++ findInOrderedSet/findInOrderedSet.js
@@ -0,0 +1,8 @@
+/*
+Suppose we had an array of n integers sorted in ascending order.
+How quickly could we check if a given integer is in the array?
+*/
+
+var findInOrderedSet = function(array) {
+  // TODO
+}
