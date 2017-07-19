32dc2e0390b0958d89dd8c243a5b9db86ca4fe20
Franklin Jeng <franklinjjeng@gmail.com>
2017-07-19 10:38:17 -0700

    adding problem nthFibonacci

---
+++ nthFibonacci/nthFibonacci.js
@@ -0,0 +1,14 @@
+/*
+Write a function fib() that a takes an integer nn and returns the nnth fibonacci number.
+Let's say our fibonacci series is 0-indexed and starts with 0. So:
+
+fib(0); // => 0
+fib(1); // => 1
+fib(2); // => 1
+fib(3); // => 2
+fib(4); // => 3
+*/ 
+
+var fib = function(n, result) {
+  // TODO
+}
