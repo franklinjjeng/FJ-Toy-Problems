3f051d8bdcb4c8663dd65118b5d8f3f0c190c979
Franklin Jeng <franklinjjeng@gmail.com>
2017-07-23 11:29:23 -0700

    adding problem reverseSubarraySort

---
+++ reverseSubarraySort/reverseSubarraySort.js
@@ -0,0 +1,17 @@
+/*
+Given an unsorted array, if you are able to reverse one sub array to sort the array in ascending order, return true.
+
+example: 
+input: [1, 2, 5, 4, 3, 10]
+output: true
+as the sub array[5, 4, 3] can be reversed to become [1, 2, 3, 4, 5, 10]
+
+input: [1, 2, 5, 3, 6, 4]
+output: false
+You cannot reverse one sub array to sort the array
+
+*/
+
+var reverseSubarraySort = function(array) {
+  // TODO
+}
