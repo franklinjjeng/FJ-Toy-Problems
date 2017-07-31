7004b78361d9e212dc0b3a2ecf039db72e37cf9e
Franklin Jeng <franklinjjeng@gmail.com>
2017-07-31 10:17:40 -0700

    adding problem singleRiffShuffle

---
+++ singleRiffShuffle/singleRiffShuffle.js
@@ -0,0 +1,11 @@
+/*
+Write a function to tell us if a full deck of cards shuffledDeck is a single riffle of two other halves half1 and half2.
+*/
+
+var buildDeck = function() {
+  // TODO
+}
+
+var singleRiffShuffle = function() {
+  // TODO
+}
