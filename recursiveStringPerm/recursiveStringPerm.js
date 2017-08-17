bd325227af1889eb2fd7c88eb62b5a3ceec442a4
Franklin Jeng <franklinjjeng@gmail.com>
2017-08-17 11:49:24 -0700

    completed recursiveStringPerm

--- recursiveStringPerm/recursiveStringPerm.js
+++ recursiveStringPerm/recursiveStringPerm.js
@@ -3,9 +3,27 @@ Write a recursive function for generating all permutations of an input string. R
 No constraints on time or space complexity
 */
 
-var recursiveStringPerm = function(string) {
+var recursiveStringPerm = function(string, current, result) {
+  current = current || [];
+  result = result || [];
+
+  if (current.length === string.length) {
+    result.push(current.slice().join(''));
+    return;
+  }
   
+  for (var i = 0; i < string.length; i++) {
+    var char = string.charAt(i);
+    if (!current.includes(char)) {
+      current.push(char);
+      recursiveStringPerm(string, current, result);
+      current.pop();
+    }
+  }
+
+  return result;
 }
+
 
 console.log(recursiveStringPerm('ab')); // ['ab', 'ba']
 console.log(recursiveStringPerm('abc')); // ['abc', 'acb', 'bac', 'bca', 'cab', 'cba']
