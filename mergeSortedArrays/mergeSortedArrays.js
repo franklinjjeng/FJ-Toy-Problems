95be52263daeb64358b58a4c4cbc6cd82bc67c0c
Franklin Jeng <franklinjjeng@gmail.com>
2017-08-17 12:24:19 -0700

    adding problem mergeSortedArrays

---
+++ mergeSortedArrays/mergeSortedArrays.js
@@ -0,0 +1,16 @@
+/*
+We have our lists of orders sorted numerically already, in arrays.
+Write a function to merge our arrays of orders into one sorted array.
+
+For example:
+
+var myArray     = [3, 4, 6, 10, 11, 15];
+var alicesArray = [1, 5, 8, 12, 14, 19];
+
+console.log(mergeArrays(myArray, alicesArray));
+// logs [1, 3, 4, 5, 6, 8, 10, 11, 12, 14, 15, 19]
+*/
+
+var mergeArrays = function(nums1, nums2) {
+  // TODO
+}
