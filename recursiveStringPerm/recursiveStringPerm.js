fc778e767c117e68b0a60e34e0754b9071c0693c
Franklin Jeng <franklinjjeng@gmail.com>
2017-08-17 11:27:00 -0700

    adding basic tests for recursiveStringPerm

---
+++ recursiveStringPerm/recursiveStringPerm.js
@@ -0,0 +1,11 @@
+/*
+Write a recursive function for generating all permutations of an input string. Return them as a set.
+No constraints on time or space complexity
+*/
+
+var recursiveStringPerm = function(string) {
+  
+}
+
+console.log(recursiveStringPerm('ab')); // ['ab', 'ba']
+console.log(recursiveStringPerm('abc')); // ['abc', 'acb', 'bac', 'bca', 'cab', 'cba']
