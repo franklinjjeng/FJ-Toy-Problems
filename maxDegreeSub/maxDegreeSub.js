fd63b26334c297bf411ca7f894587f6a9399b47b
Franklin Jeng <franklinjjeng@gmail.com>
2017-10-06 10:01:55 -0700

    adding problem maxDegreeSub

---
+++ maxDegreeSub/maxDegreeSub.js
@@ -0,0 +1,15 @@
+/*
+Shortest Sub-array Containing Maximum Degree
+Given an array of numbers, the degrees is the number of times the same number shows up. For example: [1, 2, 2, 3] would have a max degree of two as 2 appears twice. Find the shortest length sub-array containing the maximum number of degrees.
+
+example:
+input: [2, 2, 3, 1, 3]
+output: 2
+explanation: The maximum number of degrees is two as 2 and 3 both appear twice.
+The sub-arrays containing the maximum degrees are [2, 2] and [3, 1, 3].
+The shortest sub-array length is found to be two.
+*/
+
+var maxDegreeSub = function(nums) {
+  // TODO
+}
